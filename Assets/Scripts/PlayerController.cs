059af5ab9b293b5828e29c443307d7db5b6528f8
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-26 15:10:17 -0500

    l

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,15 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // move the vehicle forward
         transform.Translate (Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
